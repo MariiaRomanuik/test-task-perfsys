037f8d234f9fe743e35d2a4975e871c399030521
Mariia Romanuik <romanuik7meri.ua@gmail.com>
2024-04-13 01:02:54 +0300

    save extracted data to dynamodb

--- process_file/lambda_handler.py
+++ process_file/lambda_handler.py
@@ -2,12 +2,38 @@ import boto3
 
 s3 = boto3.client('s3')
 textract = boto3.client('textract')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('files-table')
 
 
-def lambda_handler(event):
-    # Get the bucket and key from the S3 event
+def create_dynamodb_item(file_id, text):
+    table.put_item(
+        Item={'fileid': file_id, 'extracted_text': text}
+    )
+
+
+def update_dynamodb_item(file_id, text):
+    table.update_item(
+        Key={'fileid': file_id},
+        UpdateExpression='SET extracted_text = :val',
+        ExpressionAttributeValues={':val': text}
+    )
+
+
+def get_dynamodb_item(file_id):
+    response = table.get_item(Key={'fileid': file_id})
+    return response.get('Item')
+
+
+def get_bucket_and_key(event):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
+    return bucket, key
+
+
+def lambda_handler(event, lambda_context):
+    # Get the bucket and key from the S3 event
+    bucket, key = get_bucket_and_key(event)
 
     response = textract.detect_document_text(
         Document={'S3Object': {'Bucket': bucket, 'Name': key}}
@@ -19,7 +45,21 @@ def lambda_handler(event):
         if item['BlockType'] == 'LINE':
             text += item['Text'] + '\n'
 
+        # Get the file_id from the key (assuming file_id is part of the key)
+        file_id = key.split('/')[0]
+
+        # Check if the record with file_id already exists in DynamoDB
+        existing_record = get_dynamodb_item(file_id)
+
+        if existing_record:
+            # If the record exists, update it with the extracted text
+            update_dynamodb_item(file_id, text)
+        else:
+            # If the record does not exist, create a new record with the extracted text
+            create_dynamodb_item(file_id, text)
+
     return {
-        'statusCode': 200,
-        'body': text
-    }
+            'statusCode': 200,
+            'body': text
+        }
+
