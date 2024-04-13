596199dd83767b9cce2bb62e153e55ef75da2471
Mariia Romanuik <romanuik7meri.ua@gmail.com>
2024-04-13 22:15:40 +0300

    add handling exception for make callback lambda

--- makeCallback/lambda_handler.py
+++ makeCallback/lambda_handler.py
@@ -1,20 +1,24 @@
 import json
 import requests
 from lambda_handlers.handlers.lambda_handler import LambdaContext, Event
+from requests import RequestException
 
 
 def lambda_handler(event: Event, context: LambdaContext) -> None:
-    extracted_text = event["Records"][0]["dynamodb"]["NewImage"]["extracted_text"]["S"]
-    callback_url = event["Records"][0]["dynamodb"]["NewImage"]["callback_url"]["S"]
-    if extracted_text:
-        payload = {
-            "extracted_text": extracted_text
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        try:
-            response = requests.post(callback_url, data=json.dumps(payload), headers=headers)
-            print("POST response:", response.text)
-        except Exception as e:  # TODO: add more specific exception
-            print("Error making POST call:", e)
+    try:
+        extracted_text = event["Records"][0]["dynamodb"]["NewImage"]["extracted_text"]["S"]
+        callback_url = event["Records"][0]["dynamodb"]["NewImage"]["callback_url"]["S"]
+        if extracted_text:
+            payload = {
+                "extracted_text": extracted_text
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
+            try:
+                response = requests.post(callback_url, data=json.dumps(payload), headers=headers)
+                print("POST response:", response.text)
+            except RequestException as e:
+                print("Error making POST call:", e)
+    except KeyError as e:
+        print("KeyError:", e)
